e2ba3afb75c1ab376b13e34b1ff9cb058b14cd79
ronyhe <ronyhe@gmail.com>
2019-11-27 21:32:40 +0200

    Added another describe

--- src/leash.spec.js
+++ src/leash.spec.js
@@ -4,29 +4,31 @@ const SOME_NUMBER = 5
 const SOME_OTHER_NUMBER = SOME_NUMBER + 7
 
 describe('API', () => {
-    it('has accessors', () => {
-        expectAccessors(leash())
-    })
+    describe('Atom', () => {
+        it('has accessors', () => {
+            expectAccessors(leash())
+        })
 
-    it('returns its own value on get', () => {
-        expectGet(leash(SOME_NUMBER)).toBe(SOME_NUMBER)
-    })
+        it('returns its own value on get', () => {
+            expectGet(leash(SOME_NUMBER)).toBe(SOME_NUMBER)
+        })
 
-    it('returns a new leash on set, and does not alter the original', () => {
-        const original = leash(SOME_NUMBER)
-        const changed = original.set(SOME_OTHER_NUMBER)
+        it('returns a new leash on set, and does not alter the original', () => {
+            const original = leash(SOME_NUMBER)
+            const changed = original.set(SOME_OTHER_NUMBER)
 
-        expectGet(original).toBe(SOME_NUMBER)
-        expectGet(changed).toBe(SOME_OTHER_NUMBER)
-    })
+            expectGet(original).toBe(SOME_NUMBER)
+            expectGet(changed).toBe(SOME_OTHER_NUMBER)
+        })
 
-    it('returns a new leash on update, and does not alter the original', () => {
-        const plusOne = x => x + 1
-        const original = leash(SOME_NUMBER)
-        const changed = original.update(plusOne)
+        it('returns a new leash on update, and does not alter the original', () => {
+            const plusOne = x => x + 1
+            const original = leash(SOME_NUMBER)
+            const changed = original.update(plusOne)
 
-        expectGet(original).toBe(SOME_NUMBER)
-        expectGet(changed).toBe(SOME_NUMBER + 1)
+            expectGet(original).toBe(SOME_NUMBER)
+            expectGet(changed).toBe(SOME_NUMBER + 1)
+        })
     })
 })
 
